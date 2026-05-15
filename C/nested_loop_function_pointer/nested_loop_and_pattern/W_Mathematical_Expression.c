20777304c7ca775b13a25e159a45cd9bf7b269c8
Your Name <your.email@example.com>
2026-05-15 18:36:30 +0600

    mathmetical expression

---
+++ C/nested_loop_function_pointer/nested_loop_and_pattern/W_Mathematical_Expression.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    char s,eq;
+    scanf("%d %c %d %c %d", &a, &s, &b, &eq, &c);
+    int ans;
+    if(s == '+')
+    {
+        ans=a+b;
+    }
+    else if (s == '-')
+    {
+        ans=a-b;
+    }
+    else if (s == '*')
+    {
+        ans=a*b;
+    }
+    
+    if(ans==c){
+        printf("Yes");
+    }
+    else{
+        printf("%d", ans);
+    }
+}
